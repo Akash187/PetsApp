19d14537299cbabb2d1f090ce3619315c35e0661
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-10-03 02:29:40 +0530

    serving static files.

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+app.use(express.static('static_files'));
+
+app.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000');
+});
